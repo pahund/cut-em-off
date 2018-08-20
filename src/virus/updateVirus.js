6e10474d935f246a2971d1330810ef3c7acb51f8
Patrick Hund <pahund@team.mobile.de>
2018-08-20 21:27:22 +0200

    #3 virus changes direction depending on where it has not already been; added unit tests

--- src/virus/updateVirus.js
+++ src/virus/updateVirus.js
@@ -1,6 +1,11 @@
 import { switchDirection, isIntersection, directionSwitchMap, getOppositeDirection } from '../directions';
 import { transformMapCoordinates, getRandomInt } from '../utils';
-import { isInTheMiddle, moveVirus } from './utils';
+import { isInTheMiddle, moveVirus, getBestDirection } from './utils';
+import { mapWidth, mapHeight, tileWidth, tileHeight } from '../config';
+
+const visits = Array(mapHeight)
+    .fill()
+    .map(() => Array(mapWidth).fill(0));
 
 export default sprite => {
     let { direction, x, y } = sprite;
@@ -8,10 +13,14 @@ export default sprite => {
     const { mapX, mapY } = moveVirus(sprite);
     if (isInTheMiddle({ mapX, mapY })) {
         const tile = map.tileAtLayer('main', { x, y });
+        const col = mapX / tileWidth + 1;
+        const row = mapY / tileHeight + 1;
+        visits[row - 1][col - 1] = visits[row - 1][col - 1] + 1;
         if (isIntersection(tile)) {
             const { allowed } = directionSwitchMap[tile];
             const viable = allowed.filter(dir => dir !== getOppositeDirection(direction));
-            direction = viable[getRandomInt(0, viable.length - 1)];
+            const bestDirections = getBestDirection({ viable, visits, row, col });
+            direction = bestDirections[getRandomInt(0, bestDirections.length - 1)];
         } else {
             direction = switchDirection(tile, direction);
         }
---
+++ src/virus/utils/getBestDirections.js
@@ -0,0 +1,28 @@
+import { N, E, S, W } from '../../directions';
+
+export default ({ viable, visits, row, col }) => {
+    let minVis = Number.MAX_SAFE_INTEGER;
+    return viable
+        .map(dir => {
+            let vis;
+            switch (dir) {
+                case N:
+                    vis = visits[row - 2][col - 1];
+                    break;
+                case E:
+                    vis = visits[row - 1][col];
+                    break;
+                case S:
+                    vis = visits[row][col - 1];
+                    break;
+                case W:
+                    vis = visits[row - 1][col - 2];
+                    break;
+                default:
+            }
+            minVis = vis < minVis ? vis : minVis;
+            return { dir, vis };
+        })
+        .filter(({ vis }) => vis === minVis)
+        .map(({ dir }) => dir);
+};
